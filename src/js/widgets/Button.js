1f28fdcb094eaea1844600bd48e6ddaae33e819f
pengwan <pwan@aerohive.com>
2015-09-25 14:55:34 +0800

    button click event

--- src/js/widgets/Button.js
+++ src/js/widgets/Button.js
@@ -1,9 +1,15 @@
 var React = require('react');
 
 module.exports = React.createClass({
+    
+    _handleClick : function(e){
+        
+        this.props.onBtnClick(e);
+    },
+
     render : function(){
         return (
-            <span className="ui-btn ui-btn-blue">{this.props.label}</span>    
+            <span onClick={this._handleClick} className="ui-btn ui-btn-blue">{this.props.label}</span>    
         );
     } 
 });
--- src/js/widgets/DishItem.js
+++ src/js/widgets/DishItem.js
@@ -17,13 +17,17 @@ module.exports = React.createClass({
         this.setState({active : false});
     },
 
+    _handleBtnClick : function(e){
+        
+    },
+
     render : function(){
         return (
             <li className={this.state.active ? 'dish-list-item active' : 'dish-list-item'} onMouseEnter={this._handleMouseOver} onMouseLeave={this._handleMouseOut}>
                 <img src={this.props.src} />
                 <div className="dish-list-item-mask">
                     <p className="dish-list-item-des">{this.props.name}</p>
-                    <Button label="Book" />
+                    <Button label="Book" onBtnClick={this._handleBtnClick} />
                 </div>
             </li>
         );
